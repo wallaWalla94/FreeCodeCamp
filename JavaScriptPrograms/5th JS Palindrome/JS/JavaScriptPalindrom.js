7db04a9e7404d7242745963615dd1d44d9b0201a
Carlos Noe Loya Ponce de León <carlos94_noe@hotmail.com>
2024-08-31 21:23:51 -0600

    Palindrome

---
+++ JavaScriptPrograms/5th JS Palindrome/JS/JavaScriptPalindrom.js
@@ -0,0 +1,38 @@
+﻿const inputWord = document.getElementById('text-input');
+const resultLabel = document.getElementById('result');
+const checkButton = document.getElementById('check-btn');
+
+
+
+const isWord = (str) => {
+    const regex = /[^a-z]/ig;
+    const onlyAlphabet = str.replace(regex, '')
+    return onlyAlphabet;
+};
+
+const reverseWord = (word) => {
+    isWord(word);
+    const upperCase = word.toUpperCase();
+    return [...upperCase.split('')].reverse();
+};
+
+const palindromeCheck = (word) => {
+    if (!word) {
+        return "Please input a value";
+    };
+    const arrayWord = isWord(word).toUpperCase().split('');
+    const arrayReversed = reverseWord(word);
+    for (let i = 0; i < arrayWord.length; i++) {
+        if (arrayWord[i] !== arrayReversed[i]) {
+            return `${inputWord.value} is not a palindrome`;
+        }
+        return `${inputWord.value} is a palindrome`;
+    }
+};
+
+
+const onClick = () => {
+    resultLabel.innerText = palindromeCheck(inputWord.value);
+}
+
+checkButton.addEventListener("click", onClick);
